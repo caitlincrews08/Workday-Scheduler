a695268d6a88b42f552669c9217406f4146fab81
Caitlin Crews <caitlinrcrews@gmail.com>
2020-10-18 01:05:01 -0400

    Created README template and basic timeblocks

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Workday-Scheduler
 Homework 5
+
+## Link
+Here's the link to my [Workday_Scheduler](https://caitlincrews08.github.io/Workday-Scheduler/).
+
+## Description
+
+## Screenshot
+
--- script.js
+++ script.js
@@ -1,39 +1,94 @@
 $(document).ready(function() {
 
-  // date display
-  var now = moment().format("dddd, MMMM D, YYYY");
-  $("#currentDay").append(now);
+  // gets today's date
+  function getDate() {
+    var now = moment().format("dddd, MMMM D, YYYY");
+    $("#currentDay").append(now);
+  };
+  // displays date
+  getDate();
 
   // business hours array
-  var bizHours = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+  var bizHours = [
+    {
+      id: "0",
+      time: "9",
+      type: "am"
+    },
+    {
+      id: "1",
+      time: "10",
+      type: "am"
+    },
+    {
+      id: "2",
+      time: "11",
+      type: "am"
+    },
+    {
+      id: "3",
+      time: "12",
+      type: "pm"
+    },
+    {
+      id: "4",
+      time: "1",
+      type: "pm"
+    },
+    {
+      id: "5",
+      time: "2",
+      type: "pm"
+    },
+    {
+      id: "6",
+      time: "3",
+      type: "pm"
+    },
+    {
+      id: "7",
+      time: "4",
+      type: "am"
+    },
+    {
+      id: "8",
+      time: "5",
+      type: "pm"
+    },
+  ]
 
   // timeblocks
   bizHours.forEach(function(hour){
-    // creates row div
-    var rowDiv = $("<div>");
-  // adds class to each row
-    rowDiv.addClass("row");
-    console.log(rowDiv.class)
+    // creates row div for each hour
+    var hourDiv = $("<div>");
+    // adds row classs
+    hourDiv.addClass("row");
+    // appends each hours div to container
+    $(".container").append(hourDiv);
+    
+    // creates time display
+    var displayHour = $("<div>");
+    displayHour.text(hour.time);
+    displayHour.addClass("col-md-2", "hour");
+
+    // creates scheduling form
+    var scheduleHere = $("<div>");
+    scheduleHere.addClass("col-md-9", "description");
+    // creates text area for user input
+      var input = $("<textarea>");
+      input.addClass("col-md-9", "description");
+      scheduleHere.append(input);
+    
+    // creates save button
+    var save = $("<button>");
+    save.text("Save");
+    save.addClass("col-md-1", "saveBtn")
+    
 
 
-
-    rowDiv.html(hour);
-    $(".container").append(rowDiv);
+    hourDiv.append(displayHour, scheduleHere, save);
   });
 
 
 
 });
-{/* <div class="container">
-  <div class="row">
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-  </div>
-</div> */}
